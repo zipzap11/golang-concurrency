b0cdc904eaa254e88f9c772e44d6530042850cad
Francisco <fcisco.dev@gmail.com>
2021-12-06 09:44:09 +0700

    the real once

--- once.go
+++ once.go
@@ -9,14 +9,13 @@ var counter int = 0
 
 func main() {
 	group := &sync.WaitGroup{}
-	// once := &sync.Once{}
+	once := &sync.Once{}
 
 	for i := 0; i < 20; i++ {
 		go func() {
 			defer group.Done()
 			group.Add(1)
-			// once.Do(OnlyOnce)
-			OnlyOnce()
+			once.Do(OnlyOnce)
 		}()
 	}
 
